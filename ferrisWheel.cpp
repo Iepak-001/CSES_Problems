d8ece04455ba370e9634fd846787744552b919d1
Deepak <deeepak2002@gmail.com>
2025-01-01 23:04:43 +0530

    FerrisWheel  Accepted

--- ferrisWheel.cpp
+++ ferrisWheel.cpp
@@ -2,5 +2,36 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    int x;
+    cin>>n>>x;
+
+    vector<int>weights(n);
+
+    for(int i=0; i<n; i++) cin>>weights[i];
+
+    sort(weights.begin(), weights.end());
+
+    int left=0;
+    int right=n-1;
+
+    int counter=0;
+    while(left<=right){
+        int tempWeight=weights[right];
+        right--;
+        counter++;
+
+        if(tempWeight<=x){
+            if(tempWeight+weights[right] <=x){
+                tempWeight+=weights[right];
+                right--;
+            }
+            else if(tempWeight+weights[left] <=x){
+                tempWeight+=weights[left];
+                left++;
+            }
+        }
+        
+    }
+    cout<<counter;
 }
